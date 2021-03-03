704c1ac45c5d4aaf3bce72e6381c372c15b28d49
tahaelsherif <taha95elsherif@gmail.com>
2021-03-03 12:00:26 +0200

    Add strings to config file

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 // Configure your Ushahidi deployment
 //
 // Uncomment lines below to configure your deployment
-// window.ushahidi = {
-// 	backendUrl : "https://ushahidi-platform-api-release.herokuapp.com",
-// 	mapboxApiKey: ""
-// };
-window.ushahidi = {};
+window.ushahidi = {
+	backendUrl : 'https://wfp-api.herokuapp.com',
+	mapboxApiKey: ''
+};
+//window.ushahidi = {};
